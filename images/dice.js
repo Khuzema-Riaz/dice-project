e2f7dcd329fb3172556823ca9ab23590fbd9039a
khuzema-Riaz <143374097+Khuzema-Riaz@users.noreply.github.com>
2023-09-25 20:45:26 +0500

    upload. dicehtml

---
+++ images/dice.js
@@ -0,0 +1,7 @@
+var randomNumber = Math.floor(Math.random() *6) + 1;
+console.log(randomNumber);
+
+var randomDiceImage = "Dice" + randomNumber  + ".png"
+
+var randomimagesource ="image"+ randomimage;
+ 
---
+++ index.js
@@ -0,0 +1,9 @@
+var randomNumber = Math.florr(math.random() *6) +1;
+console.log(randomNumber)
+
+var randomdDiceImage = "Dice" +randomNumber  + ".png"
+ 
+var randomImageSource = "image"+ randomImage;
+
+var image1 =  document.quarryselectorAll("img")[0]
+ image1.setAttribute("src");
